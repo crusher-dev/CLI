8c24a3d8e2f363f902e79abc346cebbabd38faf8
Utkarsh <utkarshdix02@gmail.com>
2020-06-29 05:30:07 +0530

    .

--- src/utils.ts
+++ src/utils.ts
@@ -57,12 +57,13 @@ export function getGitLastCommitSHA() {
 
 export function getGitBranchName() {
   return new Promise((resolve, reject) => {
-    exec(`git for-each-ref --format='%(objectname) %(refname:short)' refs/heads | awk "/^$(git rev-parse HEAD)/ {print \\$2}"`, function(err, stdout){
-      if(err){reject(err); return;}
-
-      const branchName = stdout.toString().trim();
-      resolve(branchName);
-    });
+    resolve(process.env.GITHUB_REF);
+    // exec(`git for-each-ref --format='%(objectname) %(refname:short)' refs/heads | awk "/^$(git rev-parse HEAD)/ {print \\$2}"`, function(err, stdout){
+    //   if(err){reject(err); return;}
+    //
+    //   const branchName = stdout.toString().trim();
+    //   resolve(branchName);
+    // });
   });
 }
 
