a508ae850df3475c974f068f852c182dde809ef2
Utkarsh <utkarshdix02@gmail.com>
2020-06-29 18:05:12 +0530

    .

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const BACKEND_SERVER_URLS: {DEV:string, PROD: string}  = {
   DEV: 'http://localhost:8000',
-  PROD: 'http://d8db36b06c2a.ngrok.io'
+  PROD: 'http://d28de9988362.ngrok.io'
 };
 
 export const FRONTEND_SERVER_URLS: {DEV:string, PROD: string}  = {
