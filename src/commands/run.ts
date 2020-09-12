273b1cb57193e6a74d0bc23a5a8f1cce98e3e331
Himanshu <hudixt@gmail.com>
2020-09-12 09:21:18 +0530

    Add support for port

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -7,10 +7,10 @@ import {
   getGitRepos,
   getLastCommitName, isFromGithub
 } from '../utils';
-import Setup from './setup';
 import cli from "cli-ux";
 import fetch from "node-fetch";
-const url = require('url');
+import url from "url";
+import localtunnel from "localtunnel";
 
 export default class Run extends Command {
   static description = 'Run visual diff'
@@ -28,27 +28,35 @@ export default class Run extends Command {
     project_id: flags.string({description: 'Project ID'}),
     endpoint: flags.string({description: 'Endpoint of the crusher server [Optional][Debugging]'}),
     tunnel: flags.boolean({char: 't', description: 'Enable tunneling for remote machine'}),
+    port: flags.string({description: 'Port for tunnelling to work'}),
   }
 
   async run() {
     const { flags} = this.parse(Run);
-    const {base_url, test_ids, project_id, crusher_token, tunnel, endpoint} = flags;
-
+    const {test_ids, project_id, crusher_token, tunnel, endpoint, port} = flags;
+    let {base_url} = flags;
     if (typeof (test_ids) === 'undefined' && typeof (project_id) === 'undefined') {
       console.log('Either test ID or Test Group IDs are needed to run the test.')
       return
     }
 
-    if (typeof (base_url) === 'undefined' && !tunnel) {
-      console.log('Please enter base url or use tunnelling')
+    if (typeof (base_url) === 'undefined' && !(tunnel && port)) {
+      console.log('Please enter base url or tunnelling/port')
     }
 
     await cli.action.start('Starting visual test')
     await new Promise(r => setTimeout(r, 1000))
 
     if (tunnel) {
-      await cli.action.start('Creating local tunnel')
-      await new Promise(r => setTimeout(r, 1000))
+      cli.action.start('Creating local tunnel')
+      const tunnel = await localtunnel({ port: port || 80 });
+      cli.action.start('Creating local tunnel')
+      base_url = tunnel.url;
+
+      tunnel.on('close', () => {
+        console.log("Tunnel connection close unexpectedly.")
+      });
+
     }
 
     const gitSha = await getGitLastCommitSHA();
@@ -59,12 +67,13 @@ export default class Run extends Command {
 
     const firstRepoName = (Object.values(gitRepos)[0] as any).fetch;
     if(project_id && !test_ids) {
-      //@ts-ignore
 
       const response = await fetch(url.resolve(endpoint ? endpoint : getBackendServerUrl(), `/projects/runTests/${project_id}`), {
         method: "POST",
-        headers: {				Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json',},
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+          },
         body: JSON.stringify({
           cliToken: crusher_token,
           projectId: project_id,
@@ -75,6 +84,8 @@ export default class Run extends Command {
           isFromGithub: isRunningFromGithub,
           repoName: await extractRepoFullName(firstRepoName)
         })}).then(res => {return res.json();});
+
+
       if(response && response.status === "RUNNING_TESTS"){
         console.log("Test have started running");
       } else {
--- src/commands/setup.ts
+++ src/commands/setup.ts
@@ -1,33 +1,28 @@
-import {Command, flags} from '@oclif/command'
+import {Command, flags} from "@oclif/command"
 import {getBackendServerUrl, getFrontendServerUrl, getUniqueString} from "../utils";
-import base = Mocha.reporters.base;
 const {cli} = require('cli-ux')
 const fetch = require('node-fetch');
-const { prompt,MultiSelect,Confirm } = require('enquirer');
+const { prompt,MultiSelect} = require('enquirer');
 
 export default class Setup extends Command {
   static description = 'Run visual diff'
-
   static userLoginCheckInterval: any = null;
-
   static examples = [
     `Generate config for running commands
     `,
   ]
-
   static flags = {
     help: flags.help({char: 'h'}),
   }
 
   static userLoginCheckPoll: any = null;
-
   static randomGeneratedToken = getUniqueString();
   static userData:any = null;
 
   static waitForUserLogin = ():Promise<string> => {
-    return new Promise((resolve,reject) => {
+    return new Promise((resolve) => {
+
        const userLoginCheckPoll = async () : Promise<any> => {
-         // console.log(getBackendServerUrl());
         const response = await fetch(`${getBackendServerUrl()}/cli/status/${Setup.randomGeneratedToken}`).then(res => res.json());
         response.status==='Completed' && resolve(response);
       }
@@ -47,6 +42,7 @@ export default class Setup extends Command {
     await cli.open(`${getFrontendServerUrl()}?cli_token=${Setup.randomGeneratedToken}`)
     Setup.userData =  await Setup.waitForUserLogin();
     await cli.action.stop();
+
     if(Setup.userLoginCheckPoll){
       clearInterval(Setup.userLoginCheckPoll);
       Setup.userLoginCheckPoll = null;
@@ -71,7 +67,9 @@ export default class Setup extends Command {
 
     const selectedProjectId = selectedProjectOption[Object.keys(selectedProjectOption)[0]];
     const selectedProject = Setup.userData.projects.find((project) => project.id === selectedProjectId);
+
     const {shouldRunIndividualTest} = await prompt({type: 'confirm', initial: 'N', name: 'shouldRunIndividualTest', message: 'Do you want to run individual test?'})
+
     if (shouldRunIndividualTest) {
       const {projectTestList} = selectedProject;
 
@@ -91,7 +89,6 @@ export default class Setup extends Command {
 
       const selectTestOption =  await selectTestOptionPropmpt.run();
 
-
       const testIds = Object.keys(selectTestOption).map((key)=>{
         return selectTestOption[key]
       })
@@ -106,24 +103,32 @@ export default class Setup extends Command {
      return `--project_id=${selectedProjectId}`
   }
 
-  async runLocally() {
+  async addHostInfo() {
+
     const {shouldRunLocally} = await prompt({type: 'confirm', name: 'shouldRunLocally', message:'Do you run test for locally hosted website?'})
+
     // If not ask for base host
     if (!shouldRunLocally) {
       const {host} = await prompt({type: 'input', name: 'host', message: 'Override Host (Skip if No)'});
       if(host.trim() != '') {
         return `--base_url=${host}`
-      } else {
-        return '';
       }
+        return '';
     }
-    return ' -t'
+
+    // Ask for port
+      const {port} = await prompt({type: 'input', name: 'port', message: 'Port exposed to run locally'});
+      if(port.trim() != '') {
+        return `-t --port=${port}`
+      }
+
+    return '-t';
   }
 
   async run() {
     const crusherTokenFlag = await this.userLogin()
     const testIDsFlag = await this.selectTests()
-    const hostParamFlag = await this.runLocally()
+    const hostParamFlag = await this.addHostInfo()
     const generatedCommand = `./bin/run run ${testIDsFlag} ${hostParamFlag} ${crusherTokenFlag} `
 
     console.log('\n\n 💁💁 Please use following command to run test\n\n')
