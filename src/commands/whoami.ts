12b74af9c8d27c646b25ae35313ceb6313fa129c
Utkarsh Dixit <utkarsh@crusher.dev>
2022-02-02 22:53:46 +0530

    .

--- src/commands/whoami.ts
+++ src/commands/whoami.ts
@@ -1,12 +1,18 @@
 import Command from "@oclif/command";
 import { getUserInfo } from "../state/userInfo";
-import { getLoggedInUser } from "../utils/index";
+import { findGitRoot, getLoggedInUser } from "../utils/index";
+import * as ini from "ini";
+import * as fs from "fs";
 
 export default class WhoAmI extends Command {
     static description = 'Your account information';
 
     async run() {
         const userAccount = getLoggedInUser();
+        const config = fs.readFileSync(findGitRoot() + "/config", "utf-8");
+        const configFile = ini.parse(config);
+        const remotes = Object.keys(configFile).filter(key => key.startsWith("remote")).map(key => configFile[key]);
+        console.log("Suggested project name:", remotes.map(remote => remote.url.split("/").pop().replace(".git", "")).join("\n"));
         this.log("-----------");
         console.log("Team:", userAccount.teamName);
         console.log("Name:", userAccount.name);
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,8 @@
 import { getAppConfig, isCrusherConfigured } from "../common/appConfig";
 import { IUserInfo } from "../state/userInfo";
 import cli from "cli-ux";
+import * as path from "path";
+import * as fs from "fs";
 
 const getLoggedInUser = (): IUserInfo => {
     if(!isCrusherConfigured()) {
@@ -21,4 +23,33 @@ const isUserLoggedIn = () => {
     return !!config.userInfo;
 };
 
-export {getLoggedInUser, isUserLoggedIn};
+function findGitRoot(_start = null) {
+    let start: any = _start || process.cwd()
+    if (typeof start === 'string') {
+      if (start[start.length - 1] !== path.sep) {
+        start += path.sep
+      }
+      start = path.normalize(start)
+      start = start.split(path.sep)
+    }
+    if (!start.length) {
+      throw new Error('.git/ not found in path')
+    }
+    start.pop()
+    var dir = start.join(path.sep)
+    var fullPath = path.join(dir, '.git')
+    if (fs.existsSync(fullPath)) {
+        if(!fs.lstatSync(fullPath).isDirectory()) {
+        var content = fs.readFileSync(fullPath, { encoding: 'utf-8' })
+        var match = /^gitdir: (.*)\s*$/.exec(content)
+        if (match) {
+          return path.normalize(match[1])
+        }
+      }
+      return path.normalize(fullPath)
+    } else {
+      return findGitRoot(start)
+    }
+  }
+
+export {getLoggedInUser, isUserLoggedIn, findGitRoot};
