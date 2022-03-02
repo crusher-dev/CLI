bd494622bbbc86cc5aafad2e2b45fc98ebb5567c
Himanshu <hudixt@gmail.com>
2022-03-02 11:19:07 +0530

    .

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,8 +1,9 @@
 import EntryCommand from "../commands/index";
 
-const args = process.argv.slice(2);;
+const args = process.argv.slice(2);
+const helpArgs = ["-h","--h","help","--help","-help"]
 
-if (args.length === 0) {
+if (args.length === 0 || helpArgs.includes(args[0])) {
   console.log("Choose a command to run")
   new EntryCommand().help()
 }
