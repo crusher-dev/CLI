c727e4e0c1dbd0c2cacb17761452a40c9a43ab6a
Utkarsh <utkarsh@crusher.dev>
2022-04-24 23:37:45 +0530

    Show warning if node version is below 10.0.0

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,11 +1,21 @@
 import EntryCommand from "../commands/index";
 
-const args = process.argv.slice(2);
-const helpArgs = ["-h", "--h", "help", "--help", "-help"];
+const nodeVersion = process.version.match(/^v(\d+\.\d+)/)[1];
 
-if (args.length === 0 || helpArgs.includes(args[0])) {
-  console.log("Choose a command to run");
-  new EntryCommand().help();
+if (parseFloat(nodeVersion) >= 10.0) {
+  const args = process.argv.slice(2);
+  const helpArgs = ["-h", "--h", "help", "--help", "-help"];
+
+  if (args.length === 0 || helpArgs.includes(args[0])) {
+    console.log("Choose a command to run");
+    new EntryCommand().help();
+  } else {
+    new EntryCommand().run();
+  }
+
 } else {
-  new EntryCommand().run();
+  console.error(
+    "Node version must be >= 10.0.0. You are using version: " +
+    nodeVersion
+  );
 }
